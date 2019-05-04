95d8d942e924c198634f72f890f163b3c93e7b8d
Vladislav <vlad2703123@gmail.com>
2019-05-04 17:44:37 +0300

    Дз

--- js/tempCodeRunnerFile.js
+++ js/tempCodeRunnerFile.js
@@ -1,2 +1,26 @@
-let arr = ['plum.png','orange.jpg','apple.bmp'];
-console.log(arr[2])
+var money=prompt("Ваш бюджет на месяц:",' ');
+console.log(money);
+var time=prompt("Ваша дата рождения-Введите дату в формате YYYY-MM-DD:",'');
+console.log(time);
+let AppData={
+    budget:money,
+    timeData:time,
+    expenses:{
+        MandatoryExpenses1:prompt("Введите обязательную статью расходов в этом месяце",""),
+        WillCost1:prompt("Во сколько обойдется"),
+        
+        MandatoryExpenses2:prompt("Введите обязательную статью расходов в этом месяце",""),
+        WillCost2:prompt("Во сколько обойдется"),
+
+        MandatoryExpenses1:WillCost1,
+        MandatoryExpenses2:WillCost2,
+
+    },
+
+    optionalExpenses:{},
+    incom:[],
+    savings:false
+    
+
+};
+alert(AppData.budget/30);
