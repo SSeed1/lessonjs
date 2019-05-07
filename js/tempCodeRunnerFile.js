808f5686e1a82e547a02cd1df5958497708b8ab9
Vladislav <vlad2703123@gmail.com>
2019-05-08 00:10:46 +0300

    ДЗ

--- js/tempCodeRunnerFile.js
+++ js/tempCodeRunnerFile.js
@@ -1,27 +1,36 @@
 'use strict';
-var money=prompt("Ваш бюджет на месяц:",' ');
+var money=+prompt("Ваш бюджет на месяц:",' ');
 console.log(money);
-var time=prompt("Ваша дата рождения-Введите дату в формате YYYY-MM-DD:",'');
+var time=prompt("Введите дату в формате YYYY-MM-DD:",'');
 console.log(time);
 let AppData={
     budget:money,
     timeData:time,
-    expenses:{
-        MandatoryExpenses1:prompt("Введите обязательную статью расходов в этом месяце",""),
-        WillCost1:prompt("Во сколько обойдется"),
-        
-        MandatoryExpenses2:prompt("Введите обязательную статью расходов в этом месяце",""),
-        WillCost2:prompt("Во сколько обойдется"),
-
-        MandatoryExpenses1:WillCost1,
-        MandatoryExpenses2:WillCost2,
-
-    },
-
     optionalExpenses:{},
     incom:[],
-    savings:false
-    
+    savings:false,
+    expenses:{},  
+};   
+for(i=0;i<2;i++){
+    let a=prompt("Введите обязательную статью расходов в этом месяце",""),
+        b=prompt("Во сколько обойдется");
 
-};
-alert(AppData.budget/30);
+        AppData.expenses[a]=b;
+        if((typeof(a))==='string'&& (typeof(a))!=null &&(typeof(b))!=null
+        &&a!=''&&b!=''&&a.length<50){
+            console.log("done");
+            alert(AppData.budget/30);
+        }
+}
+    AppData.moneyperDay=AppData.budget/30;
+    alert("Ежедневный бюджет"+AppData.moneyperDay);
+    if(AppData.moneyperDay<100){
+        console("Минимальный уровень достатка");
+    }else if(AppData.moneyperDay>100 &&AppData.moneyperDay<2000){
+        console.log("Средний достатка");
+    } else if (AppData.moneyperDay>2000){
+        console.log("Высокий достатка");
+    }else{
+        console.log("Ошибка");
+    }
+
